17ffc408efff4d88e0ccfca67a2289c315cd9e96
grzegorz bylica <grzegorz.bylica@gmail.com>
2015-07-11 18:09:37 +0200

    change #1

---
+++ gb-filter-json.js
@@ -0,0 +1,9 @@
+/**
+* gb-filter-json
+* @module gb-filter-json
+* @autor grzegorz.bylica (grzegorz.bylica.code@gmail.com)
+**/
+
+var gb-filter-json = function (req,data) {
+  console.log('--filter--');
+}
